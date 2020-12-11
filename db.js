729926b307a92844d846d6711a06e1d34373b58d
poiugit <poiuwk@gmail.com>
2020-12-11 20:10:37 +0900

    finished form pages Added test DB

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id:324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Daniel",
+      email: "poiuwk@gmail.com"
+    }
+  },
+  {
+    id:555555,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Daniel",
+      email: "poiuwk@gmail.com"
+    }
+  },
+  {
+    id:246890,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Daniel",
+      email: "poiuwk@gmail.com"
+    }
+  },
+  {
+    id:908765,
+    title: "Video cool",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Daniel",
+      email: "poiuwk@gmail.com"
+    }
+  }
+];
