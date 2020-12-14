4ba528b93f49079355ab00194fa0a65f386796a1
poiugit <poiuwk@gmail.com>
2020-12-14 20:05:10 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
+import mongoose from "mongoose";
+
+
+mongoose.connect(
+  "mongodb://localhost:27017/we-tube",
   {
-    id:324393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Daniel",
-      email: "poiuwk@gmail.com"
-    }
-  },
-  {
-    id:555555,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Daniel",
-      email: "poiuwk@gmail.com"
-    }
-  },
-  {
-    id:246890,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Daniel",
-      email: "poiuwk@gmail.com"
-    }
-  },
-  {
-    id:908765,
-    title: "Video cool",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Daniel",
-      email: "poiuwk@gmail.com"
-    }
+    useNewUrlParse: true,
+    useFindAndModify: false
   }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB")
+const handleError = (error) => console.log("❌ Error on DB connection:${error}")
+
+db.once("open", handleOpen);
+db.on("error", handleError);
