fde6d76701817c3da8cd0f385e91aab105b05041
poiugit <poiuwk@gmail.com>
2020-12-14 17:56:22 +0900

    More controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
-import {videos} from "../db"
+import { videos } from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
@@ -12,7 +13,17 @@ export const search = (req, res) => {
 };
 
 
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => 
+  res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, decription }
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393))
+};
+
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 
