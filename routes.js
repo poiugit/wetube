b94d5c307c56e9cb08b8d0a7a5346e382d6cfcb3
poiugit <poiuwk@gmail.com>
2020-12-16 02:06:33 +0900

    Deleting Video

--- routes.js
+++ routes.js
@@ -45,14 +45,20 @@ const routes = {
       return VIDEO_DETAIL;
     }
   },
-  editVideo: (id) =>{
+  editVideo: (id) => {
     if(id){
       return `/videos/${id}/edit`
     } else {
       return EDIT_VIDEO;
     }
   },
-  deleteVideo: DELETE_VIDEO
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  }
 };
 
 export default routes;
