ead893d494aca97a8b925ac5019d23effcd2f076
poiugit <poiuwk@gmail.com>
2020-12-16 12:26:11 +0900

    Installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,11 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
+// Home
+
 export const home = async(req, res) => {
   try{
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch(error){
     console.log(error);
@@ -11,13 +13,16 @@ export const home = async(req, res) => {
   }
 };
 
+// Search
+
 export const search = (req, res) => {
   const {
     query: {  term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
 
+// Upload
 
 export const getUpload = (req, res) => 
   res.render("upload", { pageTitle: "Upload" });
@@ -49,6 +54,8 @@ export const videoDetail = async (req, res) => {
   }
 };
 
+// Edit Video
+
 export const getEditVideo = async (req, res) => {
   const {
     params: {id}
@@ -75,12 +82,16 @@ export const postEditVideo = async(req, res) => {
   }
 };
 
+// Delete Video
+
 export const deleteVideo = async(req, res) => {
   const {
     params: { id }
   } = req;
   try {
     await Video.findOneAndRemove({ _id: id});
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
   res.redirect(routes.home);
 };
